b1a5df8e8baeacd743625845248e1b16423cd7d9
sujxxll <122386893+sujxxll@users.noreply.github.com>
2025-06-12 15:13:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 <script>
-  AFRAME.registerComponent('markerhandler', {
+  AFRAME.registerComponent('marker-handler', {
     init: function () {
       const marker = this.el;
-
       marker.addEventListener('markerFound', () => {
-        console.log('QR code marker found!');
+        console.log('QR marker found! Play dino roar.');
+        // e.g., play a sound: marker.querySelector('[sound]').components.sound.playSound();
       });
-
       marker.addEventListener('markerLost', () => {
-        console.log('QR code marker lost!');
+        console.log('QR marker lost!');
       });
     }
   });
 </script>
 
-<a-marker type="pattern" url="qr-marker.patt" markerhandler>
-  <!-- Model inside marker -->
+<a-marker
+  type="pattern"
+  url="data:image/png;base64,iVBORw0K..."
+  marker-handler
+>
+  <a-entity
+    gltf-model="https://example.com/dino.glb"
+    scale="0.5 0.5 0.5"
+    position="0 0 0"
+    sound="src: url(dino-roar.mp3); on: markerFound"
+  ></a-entity>
 </a-marker>
-

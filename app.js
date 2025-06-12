3c4402d0bc899dde3b4f246e2bd0c64d05351564
sujxxll <122386893+sujxxll@users.noreply.github.com>
2025-06-12 15:12:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+<script>
+  AFRAME.registerComponent('markerhandler', {
+    init: function () {
+      const marker = this.el;
+
+      marker.addEventListener('markerFound', () => {
+        console.log('QR code marker found!');
+      });
+
+      marker.addEventListener('markerLost', () => {
+        console.log('QR code marker lost!');
+      });
+    }
+  });
+</script>
+
+<a-marker type="pattern" url="qr-marker.patt" markerhandler>
+  <!-- Model inside marker -->
+</a-marker>
 

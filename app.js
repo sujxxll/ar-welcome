0551275ce4c99dcb988c434f8f8a693b076f032a
sujxxll <122386893+sujxxll@users.noreply.github.com>
2025-06-12 15:09:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// Wait for A-Frame to load
-window.addEventListener('load', () => {
-  const marker = document.querySelector('a-marker');
-  const dino = marker.querySelector('a-gltf-model');
 
-  marker.addEventListener('markerFound', () => {
-    console.log('🔍 Marker found!');
-    dino.setAttribute('visible', true);
-  });
-
-  marker.addEventListener('markerLost', () => {
-    console.log('❌ Marker lost!');
-    dino.setAttribute('visible', false);
-  });
-});
